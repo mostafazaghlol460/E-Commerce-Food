36a284bf9a17635668fe7c5a01412455dfaa795f
mostafazaghlol460 <mostafazaghlol768@gmail.com>
2023-08-13 11:45:34 +0300

    Authorize is working

--- E-CommerceFood/Program.cs
+++ E-CommerceFood/Program.cs
@@ -1,4 +1,12 @@
 
+using E_CommerceFood.BLL.Managers.UserDLL;
+using E_CommerceFood.DAL;
+using E_CommerceFood.DAL.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace E_CommerceFood
 {
     public class Program
@@ -13,7 +21,56 @@ namespace E_CommerceFood
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddScoped<UserBLL>();
+            #region Database
+            var ConnectionString = builder.Configuration.GetConnectionString("Food");
+            builder.Services.AddDbContext<DBContextFood>(options =>
+            options.UseSqlServer(ConnectionString,b=>b.MigrationsAssembly("E-CommerceFood"))
+            );
+            #endregion
+            #region IdentityMangers
 
+            builder.Services
+                .AddIdentity<ApplicationUser, IdentityRole>(options =>
+                {
+                    options.User.RequireUniqueEmail = true;
+                })
+                .AddEntityFrameworkStores<DBContextFood>();
+            #endregion
+            #region Authentication Scheme
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "Cool";
+                options.DefaultChallengeScheme = "Cool";
+            })
+            .AddJwtBearer("Cool", options =>
+            {
+                var secretKeyString = builder.Configuration.GetValue<string>("SecretKey");
+                var secretyKeyInBytes = Encoding.ASCII.GetBytes(secretKeyString ?? string.Empty);
+                var secretKey = new SymmetricSecurityKey(secretyKeyInBytes);
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = secretKey,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                };
+            });
+
+            #endregion
+            #region AddCors to Application
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
+                    });
+            });
+            #endregion
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -22,7 +79,7 @@ namespace E_CommerceFood
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
+            app.UseCors("AllowAll");
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
